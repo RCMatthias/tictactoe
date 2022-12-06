49ce2e5dfb1bfa6f9362750bd5f1570448b888fa
RCMatthias <matthias.vdvelde@hotmail.com>
2022-12-06 18:59:45 +0100

    divide in model view and controller modules factor

--- code.js
+++ code.js
@@ -1,8 +1,77 @@
-const playerFactory = (name, score) => {
-    const sayHello = () => console.log("hello" + name);
+//player factory
+
+const Player = (name, score) => {
+    const getScore = () => score;
+    const getName = () => name;
+
+    
     return {name, score, sayHello};
 }
-test 
 
-const playerOne = playerFactory("");
-console.log (playerOne.name)
+//Game Board
+
+const gameBoard = (function(doc) {
+    const rows = 3; 
+    const columns = 3;
+
+    let board = [];
+
+
+    for (let i = 0; i < rows ; i++){
+        board [i] = []; //Create subArray
+        for (let j = 0; j < columns; j++){
+        board[i].push(j)
+    }}
+    
+    const getBoard = () => board; 
+    console.log(board)
+   
+    const writeToDOM = (selector, message) => {
+        if(!!doc && "querySelector" in doc){
+        document.querySelector(selector).innerHTML = message;
+        }
+    }
+    return {
+        getBoard,
+        writeToDOM,
+    }
+})(document);
+
+console.table(gameBoard.getBoard)
+gameBoard.writeToDOM("#target", "eyaoyao");
+
+//displayController
+const displayController = (function(doc) {
+    let board = gameBoard.getBoard
+    console.log(board)
+    let gameBoardDiv = document.getElementById("gameBoardDiv")
+    console.log(gameBoardDiv)
+    const createGrid = () => {
+        //for each array item create a grid unit; 
+        board.forEach(element => {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            gameBoardDiv.appendChild(card)
+            
+        });
+        //assign the correct coordinates to each part of the newly generated grid
+
+    }
+
+
+})(document)
+
+//Game flow 
+const gameFlow = (function(doc) {
+
+
+    return
+})(document);
+
+
+
+/*
+Modules: gameboard, displaycontroller
+
+Factories: Players 
+*/
