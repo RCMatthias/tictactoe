0878513e34f77e3e98ff75237c689003cc823999
RCMatthias <matthias.vdvelde@hotmail.com>
2022-12-06 14:17:34 +0100

    test init

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ const playerFactory = (name, score) => {
     const sayHello = () => console.log("hello" + name);
     return {name, score, sayHello};
 }
-
+test 
 
 const playerOne = playerFactory("");
 console.log (playerOne.name)
