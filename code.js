55e2ff8b084d3475bd572e8232dafd32a6855abe
RCMatthias <matthias.vdvelde@hotmail.com>
2022-12-07 16:29:16 +0100

    style bg cells and create css grid

--- code.js
+++ code.js
@@ -13,9 +13,7 @@ const Player = (name, score) => {
 const gameBoard = (function(doc) {
     const rows = 3; 
     const columns = 3;
-
     const board = [];
-
 
     for (let i = 0; i < rows ; i++){
         board[i] = []; //Create subArray
@@ -25,43 +23,57 @@ const gameBoard = (function(doc) {
     
     const getBoard = () => board; 
 
-/*     console.log(board)
- */   
+    console.log(board)
+   
     const writeToDOM = (selector, message) => {
         if(!!doc && "querySelector" in doc){
         document.querySelector(selector).innerHTML = message;
         }
     }
     return {
-        board,
         getBoard,
         writeToDOM,
     }
 })(document);
-const board = gameBoard.getBoard()
-gameBoard.writeToDOM("#target", "eyaoyao");
+
+gameBoard.writeToDOM("#target", "");
+
+
+//MAKE GETVALUE func to use below 
+
 
 //displayController
-const displayController = (function(doc) {
-    console.table(board)
-    let gameBoardDiv = document.getElementById("gameBoardDiv")
-    console.log(gameBoardDiv)
-    const createGrid = () => {
-        //for each array item create a grid unit; 
-        board.forEach(element => {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            gameBoardDiv.appendChild(card)
-            
+const displayController = (function() {
+    const gameBoardDiv = document.getElementById("gameBoardDiv");
+    console.log(gameBoardDiv);
+
+    const updateScreen =() =>  {
+        //clear the board 
+        gameBoardDiv.textContent = "";
+
+        //get newest version of board & player turn
+        const board = gameBoard.getBoard();
+
+        //display player turn
+
+        //render board squares 
+
+            //for each array item create a grid unit; 
+            board.forEach(row => {
+                row.forEach((cell, index) => {
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+                cellButton.dataset.column = index;
+/*                 cellButton.textContent = cell.getValue();
+ */                gameBoardDiv.appendChild(cellButton)
+            })
         });
-        //assign the correct coordinates to each part of the newly generated grid
+            //assign the correct coordinates to each part of the newly generated grid
 
+        }
+    updateScreen();
     }
-return{ 
-    createGrid,
-
-}
-})(document)
+)()
 
 //Game flow 
 const gameFlow = (function(doc) {
