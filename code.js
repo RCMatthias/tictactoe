254b711f73ed5dd21300f2fb9fda4e9cffdd6ff3
RCMatthias <matthias.vdvelde@hotmail.com>
2022-12-22 19:57:52 +0100

    init checkwin function to working state

--- code.js
+++ code.js
@@ -19,12 +19,28 @@ const Board = () => {
         get value(){return [...gameBoard];}
     }
 }
+
+let winner = ""
+
+
 const TicTacToe = (player1Name, player2Name) => {
     let board = Board(); 
     let player1 = Player(player1Name, "X");
     let player2 = Player(player2Name, "O");
     let currentPlayer = player1;
-    
+    let arrX = [];
+    let arrO = [];
+
+    const winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
     const switchPlayers = () => {
         if(currentPlayer===player1){
             currentPlayer=player2;
@@ -33,37 +49,55 @@ const TicTacToe = (player1Name, player2Name) => {
         }
     }
     const checkWin = () => {
-        console.log(board.value);
-        checkWinningCombo();
-    }
-    const winningCombos = [
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                [0, 3, 6],
-                [1, 4, 7],
-                [2, 5, 8],
-                [0, 4, 8],
-                [2, 4, 6],
-            ];
-    const checkWinningCombo = (winCondition, symbol) => {
-        console.log(board[winCondition[0]])
-    }
+        //check board and push indices to arrX 
+        game.board.value.forEach((element, index) => {
+            if (element == "X") {
+                 arrX.push(index)
+               /* console.log(arrX) */
+            } else {
+                return
+            }
+        //go over all vlaues in the board; if they contain "X"; add that index to arrX
+        });
 
+        game.board.value.forEach((element, index) => {
+            if (element == "O") {
+                arrO.push(index)
+/*                 console.log(arrO)
+ */            } else {
+                return
+            }
 
+        //check arrX if it looks like the wincombos 
+        const isSubset = (array1, array2) => array1.every((element) => 
+        array2.includes(element));
+
+        //run for each value in winning combinations; check if arrX or arrO matches it 
+        winCombos.forEach((element) => {
+            if(isSubset(element, arrX)){
+                winner = "X"
+            } else if(isSubset(element, arrO)){
+                winner = "O"
+            }
+        })
+
+        /* console.log(arrX)
+        console.log(isSubset(winCombos, arrX)) */
+        // DO THIS FOR EACH OBJECT IN WINCOMBOS SEPARATELY (check with issubset)
+        
+        });
+ }
     const playRound = (cellNumber) => {
-        console.log(cellNumber)
         game.board.at(cellNumber).value=game.currentPlayer.icon;
-        console.table(board.value);
         switchPlayers();
         checkWin();
     }
-  
-    
+
 //THE INTERFACE (how we interact with what's inside this module/factory)
     return {
         switchPlayers,
         board,
+        get winner(){return winner;},
         playRound,
         get currentPlayer(){return currentPlayer;}
         
@@ -73,7 +107,6 @@ const TicTacToe = (player1Name, player2Name) => {
 //THE GAME CONTROLLER 
 //run the game tictactoe, which creates the players 
 const game = TicTacToe("Margaret", "Bartholomeus");
-
 //mock data 
 /* game.board.at(0).value=game.currentPlayer.icon;
  */
@@ -82,12 +115,20 @@ const game = TicTacToe("Margaret", "Bartholomeus");
     //create the square divs when the game is run (listen to the game controller?)
 const screenController = (() => {
     const boardVal = game.board.value;
-
     const gameBoardDiv = document.querySelector('.gameBoardDiv');
 
     const updateScreen = () => {
         //reset screen
         gameBoardDiv.textContent="";
+
+        //Winner fetching & posting 
+
+    const winnerDiv = document.querySelector('.winnerDiv');
+    const winnerText = document.createElement("div");
+
+
+
+    //
 
         boardVal.forEach((element, index) => {
             const cell = document.createElement("button");
@@ -95,7 +136,6 @@ const screenController = (() => {
     
         // get the content of the current element in Array and push to the screen
             const boardArrVal = boardVal.at(index);
-            console.log(boardArrVal);
             cell.textContent = boardArrVal;
 
             cell.dataset.column = index; 
@@ -106,6 +146,12 @@ const screenController = (() => {
                 if (cell.textContent == "") {
                     e.target.textContent = game.currentPlayer.icon;
                     game.playRound(dataColumn);
+
+                    winnerText.innerText = game.winner;
+                    winnerDiv.appendChild(winnerText);
+                    console.log(winnerText)
+
+
                 } else {
                     return;
                 }
@@ -117,33 +163,8 @@ const screenController = (() => {
         });
 
     };
-
     updateScreen(); 
     
    
 })();
-    /*
-    const clickHandler = () => {
-
-    }
-    clickHandler(); 
-
-
-    //listen to the array and update contents as needed 
-
-
-     return {
-        updateScreen,
-     }
-})();
    
-
-//create divs for the names input 
-    //listen to the names and get their values (interface these out)
-
-
-
-//check for wins (but we'll look at what we cna GET from the interface of the board)
-    //check if same value is in these values of the array (based on index)
-
-    */
